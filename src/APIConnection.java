8eccc71d52947bf2b99c423dbbfea65e7de3c1ad
Rodrigofms <rodrigoms.rec@gmail.com>
2024-05-10 11:39:26 -0300

    Tratamento de exceções

--- src/APIConnection.java
+++ src/APIConnection.java
@@ -7,7 +7,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public record APIConnection(String origem, String destino, double valor) {
-    public double conversor() throws IOException, InterruptedException {
+    public double conversor()  {
         String api_key = "ebeaee067ab542a7db575c3b";
         String url_str = "https://v6.exchangerate-api.com/v6/"+ api_key +"/pair/"+ origem +"/"+ destino +"/"+valor;
 
@@ -16,8 +16,13 @@ public record APIConnection(String origem, String destino, double valor) {
                 .uri(URI.create(url_str))
                 .build();
 
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response = null;
+        try{
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e){
+            throw new RuntimeException(e);
+        }
 
         JsonParser jp = new JsonParser();
         JsonObject jsonb = jp.parse(response.body()).getAsJsonObject();
